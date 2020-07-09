76d13db8e7b4d31851d248a473c88b6a7576d7b9
syuyuusyu <syuyuusyu@gmail.com>
2020-07-09 15:26:20 +0800

    closure

--- src/modules/pod/logConsole.js
+++ src/modules/pod/logConsole.js
@@ -18,89 +18,92 @@ import { host } from "../../config/api";
 @inject("rootStore")
 @observer
 class LogConsole extends Component {
-  state = {
-    logText: "",
-  };
-
-  componentDidMount() {
-    this.loadLogs();
-    this.timer = setInterval(() => {
-      var x = document.getElementById("code-mirror-editor");
-      console.log(x);
-      x.scrollTo(0, x.scrollHeight);
-    }, 1000);
-  }
-
-  componentWillUnmount() {
-    alert(1111);
-  }
-
-  loadLogs = () => {
-    const pod = this.props.rootStore.podStore.currentElement;
-    this.logEventSource = new EventSourcePolyfill(
-      `${host}/kube/watch/log/${pod.metadata.namespace}/${pod.metadata.name}/${this.props.containerName}`,
-      {
-        headers: {
-          "access-token": sessionStorage.getItem("access-token") || "",
-        },
-      }
-    );
-    this.logEventSource.onmessage = (result) => {
-      if (result && result.data) {
-        //console.log(result.data);
-        this.setState({ logText: this.state.logText + "<br/>" + result.data });
-      }
+    state = {
+        logText: "",
     };
 
-    this.logEventSource.onerror = (err) => {
-      console.log("EventSource error: ", err);
-    };
-  };
+    componentDidMount() {
+        this.loadLogs();
+        this.timer = setInterval(() => {
 
-  componentWillUnmount() {
-    if (this.logEventSource) {
-      this.logEventSource.close();
+        }, 1000);
     }
-    this.timer && clearTimeout(this.timer);
-  }
 
-  render() {
-    return (
-      <div className={"pod"} style={{ width: "100%" }}>
-        {/*<CodeMirror*/}
-        {/*    value={this.state.logText}*/}
-        {/*    options={*/}
-        {/*        {*/}
-        {/*            mode: 'diff',*/}
-        {/*            theme: 'material',*/}
-        {/*            lineNumbers: true,*/}
-        {/*        }*/}
-        {/*    }*/}
-        {/*/>*/}
+    componentWillUnmount() {
+        clearInterval(this.timer)
+    }
 
-        <div
-          className={`CodeMirror-sizer`}
-          id="code-mirror-editor"
-          dangerouslySetInnerHTML={{ __html: this.state.logText }}
-          style={{
-            paddingTop: "10px",
-            paddingLeft: "30px",
-            paddingRight: "30px",
-            margin: "0",
-            background: "rgba(0, 0, 0)",
-            color: "white",
-            minHeight: "70vh",
-            height: "70vh",
-            width: "120%",
-            overflow: "scroll",
-          }}
-        >
-          {/*{this.state.logText}*/}
-          {/*{this.state.logText}*/}
-        </div>
-      </div>
-    );
-  }
+    loadLogs = () => {
+        const pod = this.props.rootStore.podStore.currentElement;
+        this.logEventSource = new EventSourcePolyfill(
+            `${host}/kube/watch/log/${pod.metadata.namespace}/${pod.metadata.name}/${this.props.containerName}`,
+            {
+                headers: {
+                    "access-token": sessionStorage.getItem("access-token") || "",
+                },
+            }
+        );
+        var x = document.getElementById("code-mirror-editor");
+        this.logEventSource.onmessage = (result) => {
+            if (result && result.data) {
+                //console.log(result.data);
+                this.setState({ logText: this.state.logText + "<br/>" + result.data });
+                (function (_x) {
+                    _x.scrollTo(0, _x.scrollHeight);
+                })(x)
+            }
+        };
+
+        this.logEventSource.onerror = (err) => {
+            console.log("EventSource error: ", err);
+        };
+    };
+
+    componentWillUnmount() {
+        if (this.logEventSource) {
+            this.logEventSource.close();
+        }
+        this.timer && clearTimeout(this.timer);
+    }
+
+    render() {
+        return (
+            <div className={"pod"} style={{ width: "100%" }}>
+                {/*<CodeMirror*/}
+                {/*    value={this.state.logText}*/}
+                {/*    options={*/}
+                {/*        {*/}
+                {/*            mode: 'diff',*/}
+                {/*            theme: 'material',*/}
+                {/*            lineNumbers: true,*/}
+                {/*        }*/}
+                {/*    }*/}
+                {/*/>*/}
+
+                <div
+                    style={{ lineHeight: '32px' }}
+                    className={`CodeMirror-sizer`}
+                    id="code-mirror-editor"
+                    dangerouslySetInnerHTML={{ __html: this.state.logText }}
+                    style={{
+                        paddingTop: "10px",
+                        paddingLeft: "30px",
+                        paddingRight: "30px",
+                        margin: "0",
+                        background: "rgba(0, 0, 0)",
+                        color: "white",
+                        minHeight: "70vh",
+                        height: "70vh",
+                        width: "120%",
+                        overflow: "scroll",
+                    }}
+                >
+                    {/*{this.state.logText}*/}
+                    {/*{this.state.logText}*/}
+                </div>
+            </div>
+        );
+    }
 }
 
 export default LogConsole;
