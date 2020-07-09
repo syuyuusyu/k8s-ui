d0859f0ea65a24d19eedb948bddee0aa781f6863
xuchao <ssaylo@163.com>
2020-07-09 14:43:56 +0800

    修复滚动问题

--- src/config/api.js
+++ src/config/api.js
@@ -1,17 +1,18 @@
 /*
  * @Author: your name
- * @Date: 2020-07-01 13:44:24
- * @LastEditTime: 2020-07-01 13:45:53
+ * @Date: 2020-07-09 13:47:35
+ * @LastEditTime: 2020-07-09 13:54:02
  * @LastEditors: your name
  * @Description: In User Settings Edit
  * @FilePath: /k8s-ui/src/config/api.js
  */
-const API = window.API
+
+const API = window.API;
 
 /* host */
-export let host = 'http://127.0.0.1:8002';
-export let wsUrl = `ws://127.0.0.1:8002/pod-terminal`;
-export let nsUrl = 'http://127.0.0.1:8002/kube/allnamespace'
+export let host = "http://10.10.50.199:30082";
+export let wsUrl = `ws://10.10.50.199:30082/pod-terminal`;
+export let nsUrl = "http://10.10.50.199:30082/kube/allnamespace";
 
 // export let host = 'http://10.10.25.1:8002';
 // export let wsUrl = `ws://10.10.25.1:8002/pod-terminal`;
@@ -21,13 +22,8 @@ export let nsUrl = 'http://127.0.0.1:8002/kube/allnamespace'
 // export let wsUrl = `ws://172.16.11.131:8002/pod-terminal`;
 // export let nsUrl = 'http://172.16.11.131:8002/kube/allnamespace'
 
-
 if (API) {
-    host = API.host
-    wsUrl = API.wsUrl
-    nsUrl = API.nsUrl
+  host = API.host;
+  wsUrl = API.wsUrl;
+  nsUrl = API.nsUrl;
 }
-
-
-
-
--- src/modules/pod/logConsole.js
+++ src/modules/pod/logConsole.js
@@ -1,105 +1,106 @@
 /* eslint-disable */
-import React, { Component } from 'react';
-import { inject, observer } from 'mobx-react';
-import { EventSourcePolyfill } from 'event-source-polyfill';
+import React, { Component } from "react";
+import { inject, observer } from "mobx-react";
+import { EventSourcePolyfill } from "event-source-polyfill";
 //import 'codemirror/mode/verilog/verilog';
-import 'codemirror/mode/diff/diff';
+import "codemirror/mode/diff/diff";
 //
-import 'codemirror/addon/hint/show-hint.css';
-import 'codemirror/addon/hint/show-hint.js';
+import "codemirror/addon/hint/show-hint.css";
+import "codemirror/addon/hint/show-hint.js";
 // import 'codemirror/addon/hint/javascript-hint.js';
-import 'codemirror/lib/codemirror.css';
-import 'codemirror/theme/material.css';
-import 'codemirror/theme/ambiance.css';
-import '../codeMirrorStyle.css';
+import "codemirror/lib/codemirror.css";
+import "codemirror/theme/material.css";
+import "codemirror/theme/ambiance.css";
+import "../codeMirrorStyle.css";
 
-import { host } from '../../config/api'
+import { host } from "../../config/api";
 
-
-@inject('rootStore')
+@inject("rootStore")
 @observer
 class LogConsole extends Component {
+  state = {
+    logText: "",
+  };
 
-    state = {
-        logText: ''
+  componentDidMount() {
+    this.loadLogs();
+    this.timer = setInterval(() => {
+      var x = document.getElementById("code-mirror-editor");
+      console.log(x);
+      x.scrollTo(0, x.scrollHeight);
+    }, 1000);
+  }
+
+  componentWillUnmount() {
+    alert(1111);
+  }
+
+  loadLogs = () => {
+    const pod = this.props.rootStore.podStore.currentElement;
+    this.logEventSource = new EventSourcePolyfill(
+      `${host}/kube/watch/log/${pod.metadata.namespace}/${pod.metadata.name}/${this.props.containerName}`,
+      {
+        headers: {
+          "access-token": sessionStorage.getItem("access-token") || "",
+        },
+      }
+    );
+    this.logEventSource.onmessage = (result) => {
+      if (result && result.data) {
+        //console.log(result.data);
+        this.setState({ logText: this.state.logText + "<br/>" + result.data });
+      }
+    };
+
+    this.logEventSource.onerror = (err) => {
+      console.log("EventSource error: ", err);
+    };
+  };
+
+  componentWillUnmount() {
+    if (this.logEventSource) {
+      this.logEventSource.close();
     }
+    this.timer && clearTimeout(this.timer);
+  }
 
-    componentDidMount() {
-        this.loadLogs();
-        this.timer = setInterval(() => {
-            document.getElementById('code-mirror-editor').scrollIntoView(false); // 滚动日志窗口到底部
-        }, 1000)
-    }
+  render() {
+    return (
+      <div className={"pod"} style={{ width: "100%" }}>
+        {/*<CodeMirror*/}
+        {/*    value={this.state.logText}*/}
+        {/*    options={*/}
+        {/*        {*/}
+        {/*            mode: 'diff',*/}
+        {/*            theme: 'material',*/}
+        {/*            lineNumbers: true,*/}
+        {/*        }*/}
+        {/*    }*/}
+        {/*/>*/}
 
-    componentWillUnmount() {
-        alert(1111)
-    }
-
-
-    loadLogs = () => {
-        const pod = this.props.rootStore.podStore.currentElement
-        this.logEventSource = new EventSourcePolyfill(`${host}/kube/watch/log/${pod.metadata.namespace}/${pod.metadata.name}/${this.props.containerName}`, {
-            headers: {
-                'access-token': sessionStorage.getItem('access-token') || ''
-            }
-        });
-        this.logEventSource.onmessage = result => {
-            if (result && result.data) {
-                //console.log(result.data);
-                this.setState({ logText: this.state.logText + '<br/>' + result.data });
-            }
-
-
-        };
-
-        this.logEventSource.onerror = err => {
-            console.log('EventSource error: ', err);
-        };
-
-    }
-
-
-    componentWillUnmount() {
-        if (this.logEventSource) {
-            this.logEventSource.close();
-        }
-        this.timer && clearTimeout(this.timer);
-    }
-
-    render() {
-        return (
-            <div className={'pod'} style={{ width: '100%' }}>
-                {/*<CodeMirror*/}
-                {/*    value={this.state.logText}*/}
-                {/*    options={*/}
-                {/*        {*/}
-                {/*            mode: 'diff',*/}
-                {/*            theme: 'material',*/}
-                {/*            lineNumbers: true,*/}
-                {/*        }*/}
-                {/*    }*/}
-                {/*/>*/}
-
-                <div className={`CodeMirror-sizer`} id='code-mirror-editor' dangerouslySetInnerHTML={{ __html: this.state.logText }}
-                    style={{
-                        paddingTop: '10px',
-                        paddingLeft: '30px',
-                        paddingRight: '30px',
-                        margin: '0',
-                        background: 'rgba(0, 0, 0)',
-                        color: 'white',
-                        minHeight: '70vh',
-                        height: '70vh',
-                        width: '120%',
-                        overflow: 'scroll',
-                    }}>
-                    {/*{this.state.logText}*/}
-                    {/*{this.state.logText}*/}
-                </div>
-            </div>
-        );
-    }
+        <div
+          className={`CodeMirror-sizer`}
+          id="code-mirror-editor"
+          dangerouslySetInnerHTML={{ __html: this.state.logText }}
+          style={{
+            paddingTop: "10px",
+            paddingLeft: "30px",
+            paddingRight: "30px",
+            margin: "0",
+            background: "rgba(0, 0, 0)",
+            color: "white",
+            minHeight: "70vh",
+            height: "70vh",
+            width: "120%",
+            overflow: "scroll",
+          }}
+        >
+          {/*{this.state.logText}*/}
+          {/*{this.state.logText}*/}
+        </div>
+      </div>
+    );
+  }
 }
-
 
 export default LogConsole;
